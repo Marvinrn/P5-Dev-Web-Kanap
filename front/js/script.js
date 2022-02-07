d305b6414f613ccae99ae5a75a5013baadf3a652
Marvin <marvinnrn@gmail.com>
2022-02-07 04:05:55 +0100

    fetch api home page
    affichage des données de l'api dans la page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,26 @@
+const items = document.getElementById('items');
+let products;
+
+const fetchProducts = async () => {
+  products = await fetch('http://localhost:3000/api/products')
+    .then(res => res.json());
+}
+
+
+const showProducts = async () => {
+  await fetchProducts();
+
+  items.innerHTML = (
+    products.map(product => (
+      `<a href="./product.html?${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`
+    )).join('')
+  )
+}
+
+showProducts();
